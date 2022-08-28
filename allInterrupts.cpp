40a460541bab83fca6d44224e17a8b63554ae6a7
lieven <lieven.merckx@gmail.com>
2022-08-28 22:34:12 +0200

    completing

---
+++ allInterrupts.cpp
@@ -0,0 +1,243 @@
+
+/*
+
+File added to diagnose unexpected exceptions 
+
+*/
+
+extern "C" void HardFault_handler()
+{
+    while (1)
+        ;
+}
+
+extern "C" void MemManage_Handler()
+{
+    while (1)
+        ;
+}
+
+extern "C" void BusFault_Handler()
+{
+    while (1)
+        ;
+}
+
+extern "C" void UsageFault_Handler()
+{
+    while (1)
+        ;
+}
+
+extern "C" void WWDG_IRQHandler()
+{
+    while (1)
+        ;
+}
+
+extern "C" void USB_HP_CAN1_TX_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void USB_LP_CAN1_RX0_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void CAN1_RX1_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void CAN1_SCE_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void  EXTI9_5_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM1_BRK_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM1_UP_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM1_TRG_COM_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM1_CC_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM2_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void  TIM3_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void  TIM4_IRQHandler()
+{
+    while (1)
+    ;
+}
+extern "C" void I2C1_EV_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void I2C1_ER_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void I2C2_EV_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void I2C2_ER_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void SPI1_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void SPI2_IRQHandler()
+{
+    while (1)
+        ;
+}
+/*
+extern "C" void USART1_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void  USART2_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void USART3_IRQHandler()
+{
+    while (1)
+        ;
+}*/
+extern "C" void  EXTI15_10_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void RTC_Alarm_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void USBWakeUp_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM8_BRK_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM8_UP_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM8_TRG_COM_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM8_CC_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void ADC3_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void FSMC_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void SDIO_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM5_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void SPI3_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void UART4_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void UART5_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM6_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void TIM7_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void DMA2_Channel1_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void DMA2_Channel2_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void DMA2_Channel3_IRQHandler()
+{
+    while (1)
+        ;
+}
+extern "C" void DMA2_Channel4_5_IRQHandler()
+{
+    while (1)
+        ;
+}
